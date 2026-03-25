0b717bdcb2841fc2010eeaef66f05e448d5f0a5c
res200183 <res200183@gmail.com>
2026-03-25 06:13:21 +0000

    feat: add login message

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-feature_login_enabled = False
-
 def main():
     print("App started")
 
     if feature_login_enabled:
         print("Login feature is ON")
+        print("User can now log in 🚀")  # ← новая строка
     else:
         print("Login feature is OFF")
-
-if __name__ == "__main__":
-    main()
